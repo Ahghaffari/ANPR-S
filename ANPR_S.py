50506bf381195d707abba26006c741ef0f25cd97
Amirhossein <ah.ghaffari94@gmail.com>
2022-01-31 12:07:29 +0330

    bug fixed

--- ANPR_S.py
+++ ANPR_S.py
@@ -704,7 +704,7 @@ def main():
             for (x, y, w, h) in plates:
                 gray_ROI_shape = np.shape(gray_ROI)
                 plate_img = gray_ROI[max(y - PLATE_MARGIN, 1):min(y + h + PLATE_MARGIN, gray_ROI_shape[0]-1), max(x - PLATE_MARGIN, 1):min(x + w + PLATE_MARGIN, gray_ROI_shape[1]-1)]
-    f
+
                 if VERBOSE:
                     cv2.imshow("PLATE_R_" + str(CAMERA_NUM),
                                cv2.resize(plate_img, (SHOW_RESOLUTION_X, SHOW_RESOLUTION_Y)))
